78290f6abab16f68fdb95492b69b6a5ceffbdb86
Fajriawan <fajriawan@mmi-pt.com>
2023-08-21 19:16:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Outlet, Route, Routes, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+
+export const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 function App() {
   return (
+    <Routes>
+      <Route path="/" element={<Dashboard />}>
+        <Route path="/" element={<div>INI Dashboard</div>} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Dashboard() {
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Outlet />
     </div>
   );
 }
